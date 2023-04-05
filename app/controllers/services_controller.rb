86fe86c3486a22a19e24a1930b69aa02313c18dc
arishavelle18 <arishavelle18@yahoo.com>
2023-04-05 19:14:30 +0800

    adding category image

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,5 +2,8 @@ class ServicesController < ApplicationController
     before_action :require_login
     def index
         @services = Service.all.order(created_at: :desc).where("created_at >= ?",Date.current)
+        if @services
+            @category = @services[0].category
+        end
     end
 end
